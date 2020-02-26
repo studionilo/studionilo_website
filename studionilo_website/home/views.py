38768b73a8478f16795eef0fdf97fd34ba6c9c8c
vittoriopippi <219424@studenti.unimore.it>
2020-02-26 12:19:52 +0100

    Try to fix db population

--- studionilo_website/home/views.py
+++ studionilo_website/home/views.py
@@ -24,24 +24,24 @@ def awesome(request):
     if request.method == 'POST':
         try:
             payment = Payment()
-            payment.payer_email = safe_post(request, 'payer_email')
-            payment.payer_id = safe_post(request, 'payer_id')
-            payment.payer_status = safe_post(request, 'payer_status')
-            payment.first_name = safe_post(request, 'first_name')
-            payment.last_name = safe_post(request, 'last_name')
-            payment.txn_id = safe_post(request, 'txn_id')
-            payment.txn_type = safe_post(request, 'txn_type')
-            payment.mc_currency = safe_post(request, 'mc_currency')
-            payment.payment_fee = float(safe_post(request, 'payment_fee'))
-            payment.payment_gross = float(safe_post(request, 'payment_gross'))
+            payment.payer_email = request.POST.get('payer_email', '')
+            payment.payer_id = request.POST.get('payer_id', '')
+            payment.payer_status = request.POST.get('payer_status', '')
+            payment.first_name = request.POST.get('first_name', '')
+            payment.last_name = request.POST.get('last_name', '')
+            payment.txn_id = request.POST.get('txn_id', '')
+            payment.txn_type = request.POST.get('txn_type', '')
+            payment.mc_currency = request.POST.get('mc_currency', '')
+            payment.payment_fee = float(request.POST.get('payment_fee', ''))
+            payment.payment_gross = float(request.POST.get('payment_gross', ''))
             payment.payment_revenue = payment.payment_gross - payment.payment_fee
-            payment.payment_status = safe_post(request, 'payment_status')
-            payment.payment_type = safe_post(request, 'payment_type')
-            payment.item_name = safe_post(request, 'item_name')
-            payment.payment_date = datetime.strptime(safe_post(request, 'payment_date'), '%Y-%m-%dT%H:%M:%SZ')
-            payment.verify_sign = safe_post(request, 'verify_sign')
+            payment.payment_status = request.POST.get('payment_status', '')
+            payment.payment_type = request.POST.get('payment_type', '')
+            payment.item_name = request.POST.get('item_name', '')
+            payment.payment_date = datetime.strptime(request.POST.get('payment_date', ''), '%Y-%m-%dT%H:%M:%SZ')
+            payment.verify_sign = request.POST.get('verify_sign', '')
 
-            payIntent = PaymentIntent.objects.filter(payment_intent_id=safe_post(request, 'custom')).first()
+            payIntent = PaymentIntent.objects.filter(payment_intent_id=request.POST.get('custom', '')).first()
             payIntent.purchased = True
             payIntent.save()
 
@@ -64,33 +64,27 @@ def awesome(request):
 def reject(request):
     return render(request, 'home/index.html', context={'reject':True})
 
-def safe_post(request, key, default=''):
-    if key in request.POST:
-        return request.POST[request, key]
-    else:
-        return default
-
 @csrf_exempt
 def create_payment(request):
     if request.method == 'POST':
         try:
             payIntent = PaymentIntent()
-            payIntent.name = safe_post(request, 'name')
-            payIntent.email = safe_post(request, 'email') 
-            payIntent.website = safe_post(request, 'website')
-            payIntent.sn_facebook = safe_post(request, 'sn_facebook') == 'true'
-            payIntent.sn_instagram = safe_post(request, 'sn_instagram') == 'true'
-            payIntent.sn_twitter = safe_post(request, 'sn_twitter') == 'true'
-            payIntent.sn_tiktok = safe_post(request, 'sn_tiktok') == 'true'
-            payIntent.sn_linkedin = safe_post(request, 'sn_linkedin') == 'true'
-            payIntent.sn_snapchat = safe_post(request, 'sn_snapchat') == 'true'
-            payIntent.sn_pinterest = safe_post(request, 'sn_pinterest') == 'true'
+            payIntent.name = request.POST.get('name', '')
+            payIntent.email = request.POST.get('email', '') 
+            payIntent.website = request.POST.get('website', '')
+            payIntent.sn_facebook = request.POST.get('sn_facebook', '') == 'true'
+            payIntent.sn_instagram = request.POST.get('sn_instagram', '') == 'true'
+            payIntent.sn_twitter = request.POST.get('sn_twitter', '') == 'true'
+            payIntent.sn_tiktok = request.POST.get('sn_tiktok', '') == 'true'
+            payIntent.sn_linkedin = request.POST.get('sn_linkedin', '') == 'true'
+            payIntent.sn_snapchat = request.POST.get('sn_snapchat', '') == 'true'
+            payIntent.sn_pinterest = request.POST.get('sn_pinterest', '') == 'true'
 
-            if(safe_post(request, 'plan') == 'popup_pay_1'):
+            if(request.POST.get('plan', '') == 'popup_pay_1'):
                 payIntent.plan = PaymentIntent.VIDEOREPORT
-            elif(safe_post(request, 'plan') == 'popup_pay_2'):
+            elif(request.POST.get('plan', '') == 'popup_pay_2'):
                 payIntent.plan = PaymentIntent.VIDEOCOLLOQUIO
-            elif(safe_post(request, 'plan') == 'popup_pay_3'):
+            elif(request.POST.get('plan', '') == 'popup_pay_3'):
                 payIntent.plan = PaymentIntent.MEDIAMANAGER
             else:
                 payIntent.plan = PaymentIntent.UNKNOWN
@@ -104,24 +98,24 @@ def create_payment(request):
             return JsonResponse({'error_message': traceback.format_exc()})
 
         try:
-            payIntent.screen_height = safe_post(request, 'screen_height')
-            payIntent.screen_width = safe_post(request, 'screen_width')
+            payIntent.screen_height = request.POST.get('screen_height', '')
+            payIntent.screen_width = request.POST.get('screen_width', '')
 
-            payIntent.user_agent = safe_post(request, 'user_agent')
-
-            ua = parse(payIntent.user_agent)
-            payIntent.browser_family = ua.browser.family
-            payIntent.browser_version = ua.browser.version_string
-            payIntent.os_family = ua.os.family
-            payIntent.os_version = ua.os.version_string
-            payIntent.device_family = ua.device.family
-            payIntent.device_brand = ua.device.model
-            payIntent.device_model = ua.device.brand
-            payIntent.device_is_mobile = ua.is_mobile
-            payIntent.device_is_tablet = ua.is_tablet
-            payIntent.device_is_touch_capable = ua.is_touch_capable
-            payIntent.device_is_pc = ua.is_pc
-            payIntent.device_is_bot = ua.is_bot
+            payIntent.user_agent = request.POST.get('user_agent', '')
+            if len(payIntent.user_agent) > 0:
+                ua = parse(payIntent.user_agent)
+                payIntent.browser_family = ua.browser.family
+                payIntent.browser_version = ua.browser.version_string
+                payIntent.os_family = ua.os.family
+                payIntent.os_version = ua.os.version_string
+                payIntent.device_family = ua.device.family
+                payIntent.device_brand = ua.device.model
+                payIntent.device_model = ua.device.brand
+                payIntent.device_is_mobile = ua.is_mobile
+                payIntent.device_is_tablet = ua.is_tablet
+                payIntent.device_is_touch_capable = ua.is_touch_capable
+                payIntent.device_is_pc = ua.is_pc
+                payIntent.device_is_bot = ua.is_bot
             payIntent.save()
         except:
             pass
