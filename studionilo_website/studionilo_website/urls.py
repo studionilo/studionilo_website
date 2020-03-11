721ac4bf19158c34426a86584b3c650eb89bd9ae
vittoriopippi <219424@studenti.unimore.it>
2020-03-11 11:21:44 +0100

    Changed admin url

--- studionilo_website/studionilo_website/urls.py
+++ studionilo_website/studionilo_website/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('home.urls')),
-    path('admin/', admin.site.urls),
+    path('il-nascondiglio-di-Abu/', admin.site.urls),
 ]
